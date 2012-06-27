57a7b5e19c4a1eb03d07ea3bf9234b400171c0f4
kviny <kviny@kviny-VirtualBox.(none)>
2012-06-27 21:35:06 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '5c901cf42a5fab1b84d9bb96c9a7fd6d3ec664340a0330aae1b9bd600dc1abbbea30d510bae572fcb57548b91289c768cac308f2e651666412689987d12ee89f'
