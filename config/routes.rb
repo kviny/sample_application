802203d97be9ee6df99193cd90a0abee6f7f21ef
kviny <kviny@ubuntu.ubuntu-domain>
2012-09-30 20:01:50 +0400

    Added layouts, twitter bootstrap and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
 
-  get "static_pages/help"
+  get 'users/new'
 
-  get "static_pages/about"
+  root :to => 'static_pages#home'
+
+  match '/about',   to: 'static_pages#about'
+  match '/help',    to: 'static_pages#help'
+  match '/contact', to: 'static_pages#contact'
+
+  match '/signup', to: 'users#new'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
